c49133704db966846924c9916bd62f15672ac97c
Gustavo Clay <gustavoclay@gmail.com>
2018-09-16 20:45:49 -0300

    Continuar a partir do Capitulo 3 "O QUE É JAVA EE?".

---
+++ fj21-jdbc/src/br/com/caelum/jdbc/teste/TestaInsere.java
@@ -0,0 +1,26 @@
+package br.com.caelum.jdbc.teste;
+
+import java.util.Calendar;
+
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaInsere {
+
+	public static void main(String[] args) {
+
+		Contato contato = new Contato();
+		contato.setNome("Caelum");
+		contato.setEmail("contato@caelum.com.br");
+		contato.setEndereco("R. Vergueiro 3185 cj57");
+		contato.setDataNascimento(Calendar.getInstance());
+		
+		ContatoDao dao = new ContatoDao();
+		
+		dao.adiciona(contato);
+		
+		System.out.println("Gravando!");
+
+	}
+
+}
---
+++ fj21-jdbc/src/br/com/caelum/jdbc/teste/TestaLista.java
@@ -0,0 +1,25 @@
+package br.com.caelum.jdbc.teste;
+
+import java.util.List;
+
+import br.com.caelum.jdbc.dao.ContatoDao;
+import br.com.caelum.jdbc.modelo.Contato;
+
+public class TestaLista {
+
+	public static void main(String[] args) {
+
+		ContatoDao dao = new ContatoDao();
+
+		List<Contato> contatos = dao.getLista();
+
+		for (Contato contato : contatos) {
+			System.out.println("Nome: " + contato.getNome());
+			System.out.println("Email: " + contato.getEmail());
+			System.out.println("Endereço: " + contato.getEndereco());
+			System.out.println("Data de Nascimento: " + contato.getDataNascimento().getTime() + "\n");
+		}
+
+	}
+
+}
