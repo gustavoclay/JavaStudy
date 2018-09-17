592982f18a2cc56d45d1a74e312ce2418ed240b7
Gustavo Clay <gustavoclay@gmail.com>
2018-09-17 18:16:53 -0300

    Continuar capitulo 9 MVC, verificar todo

--- fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
+++ fj21-agenda/src/br/com/caelum/servlet/AdicionaContatoServlet.java
@@ -6,6 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -56,12 +57,9 @@ public class AdicionaContatoServlet extends HttpServlet {
 		ContatoDao dao = new ContatoDao();
 		dao.adiciona(contato);
 
-		// imprime contato adicionado
-		out.println("<html>");
-		out.println("<body>");
-		out.println("Contato " + contato.getNome() + " adicionado com sucesso");
-		out.println("</body>");
-		out.println("</html>");
+		// RequestDispatcher
+		RequestDispatcher rd = request.getRequestDispatcher("/contato-adicionado.jsp");
+		rd.forward(request, response);
 
 	}
 
