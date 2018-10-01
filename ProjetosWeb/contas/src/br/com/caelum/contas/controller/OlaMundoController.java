ae520c9821e31cb9960b132d47cd5f953cd971ed
Gustavo Clay <gustavoclay@gmail.com>
2018-10-01 18:26:22 -0300

    Continuar da aula 02 MVC

---
+++ ProjetosWeb/contas/src/br/com/caelum/contas/controller/OlaMundoController.java
@@ -0,0 +1,14 @@
+package br.com.caelum.contas.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class OlaMundoController {
+	
+	@RequestMapping("/olaMundoSpring")
+	public String execute() {
+		System.out.println("Executando uma lógica com Spring MVC");
+		return "ok";
+	}
+}
